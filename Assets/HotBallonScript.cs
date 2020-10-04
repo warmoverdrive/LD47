3f4f7329b10aaac3ad2c15ef73690ee779d9b07b
Ralph <ralph.dworzanski@gmail.com>
2020-10-04 23:30:16 +0200

    Collision Detection:
    This commitment is attempting to enable collision detection, however
    currently not working due to unknown causes. Following attempts were made
    
    1) Only add Box Collider 2D without Rigidbody and check if the triggers
    fired when the colliders overlapped
    
    2) Move the canvas back so the box colliders dont collide with anything else
    
    3) Two kinematic 2d rigidbodies with boxcolliders2d
    
    None of these fired the trigger

---
+++ Assets/HotBallonScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HotBallonScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("Hit1");
+    }
+
+	private void OnCollisionEnter2D(Collision2D collision)
+	{
+        Debug.Log("Hit2");
+	}
+}
