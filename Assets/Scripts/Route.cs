8b00620c4970c3e9b518405195a2dc543537f1ff
warmoverdrive <sweetsthebear@gmail.com>
2020-10-04 15:43:03 -0700

    Added Curve Markers

--- Assets/Scripts/Route.cs
+++ Assets/Scripts/Route.cs
@@ -5,9 +5,14 @@ using UnityEngine;
 public class Route : MonoBehaviour
 {
     [SerializeField]
+    private GameObject markerPrefab;
+    [SerializeField]
+    private Transform orbitOrigin;
+    [Header("Anchor/Control Points")]
+    [SerializeField]
     public Transform[] controlPoints;
-
-    private Vector2 gizmosPosition;
+    private Vector2 markerPos;
+    private List<GameObject> dashes = new List<GameObject>();
 
     public Vector2 this[int index]
     {
@@ -15,21 +20,57 @@ public class Route : MonoBehaviour
         set { controlPoints[index].position = value; }
     }
 
+    private void Start()
+    {
+        for (float t = 0; t <= 1; t += 0.15f)
+        {
+            markerPos = Mathf.Pow(1 - t, 3) * controlPoints[0].position +
+                3 * Mathf.Pow(1 - t, 2) * t * controlPoints[1].position +
+                3 * (1 - t) * Mathf.Pow(t, 2) * controlPoints[2].position +
+                Mathf.Pow(t, 3) * controlPoints[3].position;
+
+            dashes.Add(Instantiate(markerPrefab, markerPos, RotateAroundOrigin(markerPos), transform));
+            
+        }
+    }
+
+    private void Update()
+    {
+        int i = 0;
+
+        for (float t = 0; t <= 1; t += 0.15f)
+        {
+            markerPos = Mathf.Pow(1 - t, 3) * controlPoints[0].position +
+                3 * Mathf.Pow(1 - t, 2) * t * controlPoints[1].position +
+                3 * (1 - t) * Mathf.Pow(t, 2) * controlPoints[2].position +
+                Mathf.Pow(t, 3) * controlPoints[3].position;
+
+            dashes[i].transform.position = markerPos;
+            dashes[i].transform.rotation = RotateAroundOrigin(markerPos);
+            i++;
+        }
+    }
+
     private void OnDrawGizmos()
     {
         for (float t = 0; t <= 1; t += 0.05f)
         {
-            gizmosPosition = Mathf.Pow(1 - t, 3) * controlPoints[0].position +
+            markerPos = Mathf.Pow(1 - t, 3) * controlPoints[0].position +
                 3 * Mathf.Pow(1 - t, 2) * t * controlPoints[1].position +
                 3 * (1 - t) * Mathf.Pow(t, 2) * controlPoints[2].position +
                 Mathf.Pow(t , 3) * controlPoints[3].position;
 
-            Gizmos.DrawSphere(gizmosPosition, 0.25f);
+            Gizmos.DrawSphere(markerPos, 0.1f);
         }
 
-        Gizmos.DrawLine(new Vector2(controlPoints[0].position.x, controlPoints[0].position.y),
-            new Vector2(controlPoints[1].position.x, controlPoints[1].position.y));
-        Gizmos.DrawLine(new Vector2(controlPoints[2].position.x, controlPoints[2].position.y),
-            new Vector2(controlPoints[3].position.x, controlPoints[3].position.y));
+        Gizmos.DrawLine(controlPoints[0].position, controlPoints[1].position);
+        Gizmos.DrawLine(controlPoints[2].position, controlPoints[3].position);
+    }
+
+    private Quaternion RotateAroundOrigin(Vector3 newPos)
+    {
+        Vector3 difference = orbitOrigin.position - newPos;
+        float rotationZ = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
+        return Quaternion.Euler(0.0f, 0.0f, rotationZ - 90);
     }
 }
