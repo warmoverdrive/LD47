c8fb549bed5696efb9058e5c8dc5dabb64f8a414
warmoverdrive <sweetsthebear@gmail.com>
2020-10-03 21:47:45 -0700

    Controller re-work Rewrote the entire Orbit Controller script and actually got the curve points moving Known issues: -Anchor clamping works, but does not check distance on Control Points (control points overlap/cross and mess up the curve) -Control Points aren't updating for opposing move checks, and fall out of line with anchors -Square (player) does not update route between anchors and snaps between old location and new

--- Assets/OrbitController.cs
+++ Assets/OrbitController.cs
@@ -4,79 +4,106 @@ using UnityEngine;
 
 public class OrbitController : MonoBehaviour
 {
-    [SerializeField]
-    private Route[] routes;
+    [Header("Design Levers")]
     [SerializeField]
     float anchorPointMoveSpeed = 1f;
     [SerializeField]
+    float controlPointMoveSpeed = 0.25f;
+    [SerializeField]
     float minOrbitRadius = 3f;
     [SerializeField]
-    float controlPointMoveSpeed = 0.25f;
-
-    private void Start()
-    {
-        if (routes == null)
-        {
-            Debug.LogError("ERROR: NO ROUTES FOUND IN ORBIT CONTROLLER");
-        }
-    }
+    float maxOrbitRadius = 20f;
+    [Header("Anchor Points")]
+    [SerializeField]
+    [Tooltip("Array of GameObjects acting as Vertical Anchor Points. Order in Array as N then S.")]
+    private Transform[] anchors_N_S;
+    [SerializeField]
+    [Tooltip("Array of GameObjects acting as Horizontal Anchor Points. Order in Array as E then W.")]
+    private Transform[] anchors_E_W;
+    [Header("Control Points")]
+    [SerializeField]
+    [Tooltip("Array of GameObjects acting as Horizontal Control Points. Order in Array as Right then Left.")]
+    private Transform[] controlPoints_R_L;
+    [SerializeField]
+    [Tooltip("Array of GameObjects acting as Vertical Control Points. Order in Array as Up then Down.")]
+    private Transform[] controlPoints_U_D;
+    
 
     private void Update()
     {
-        float anchorDelta = anchorPointMoveSpeed * Time.deltaTime;
-        float controlPointDelta = controlPointMoveSpeed * Time.deltaTime;
-
-        //up
-        if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow)) //HORIZONTAL CONTRACT
         {
-            VerticalOrbitManipulation(anchorDelta, controlPointDelta);
+            if (Vector2.Distance(anchors_E_W[0].position, anchors_E_W[1].position) >= minOrbitRadius)
+            {
+                HorizontalOrbitAdjustment(-Time.deltaTime);
+            }
         }
-        //down
-        if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow))
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow)) //HORIZONTAL EXPAND
         {
-            VerticalOrbitManipulation(-anchorDelta, -controlPointDelta);
+            if (Vector2.Distance(anchors_E_W[0].position, anchors_E_W[1].position) <= maxOrbitRadius)
+            {
+                HorizontalOrbitAdjustment(Time.deltaTime);
+            }
         }
-        //left
-        if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow)) //VERTICAL EXPAND
         {
-            HorizontalOrbitManipulation(anchorDelta, controlPointDelta);
+            if (Vector2.Distance(anchors_N_S[0].position, anchors_N_S[1].position) <= maxOrbitRadius)
+            {
+                VerticalOrbitAdjustment(Time.deltaTime);
+            }
         }
-        //right
-        if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
+        if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow)) //VERTICAL CONTRACT
         {
-            HorizontalOrbitManipulation(-anchorDelta, -controlPointDelta);
+            if (Vector2.Distance(anchors_N_S[0].position, anchors_N_S[1].position) >= minOrbitRadius)
+            {
+                VerticalOrbitAdjustment(-Time.deltaTime);
+            }
         }
     }
 
-    private void VerticalOrbitManipulation(float anchorDelta, float controlPointDelta)
+    private void HorizontalOrbitAdjustment(float deltaTime)
     {
-        //up anchors
-        routes[0][3].Set(routes[0][3].x, routes[0][3].y + anchorDelta);
-        routes[1][0].Set(routes[1][0].x, routes[1][0].y + anchorDelta);
-        //up control points
-        routes[0][1].Set(routes[0][1].x, routes[0][1].y + controlPointDelta);
-        routes[1][2].Set(routes[1][2].x, routes[1][2].y + controlPointDelta);
-        //down anchors
-        routes[2][3].Set(routes[2][3].x, routes[2][3].y - anchorDelta);
-        routes[3][0].Set(routes[3][0].x, routes[3][0].y - anchorDelta);
-        //down control points
-        routes[3][2].Set(routes[3][2].x, routes[3][2].y - controlPointDelta);
-        routes[2][1].Set(routes[2][1].x, routes[2][1].y - controlPointDelta);
+        float anchorDelta = anchorPointMoveSpeed * deltaTime;
+        float cPointDelta = controlPointMoveSpeed * deltaTime;
+        
+        //set anchors
+        anchors_E_W[0].position = new Vector2(
+            anchors_E_W[0].position.x + anchorDelta, anchors_E_W[0].position.y); //set east
+        anchors_E_W[1].position = new Vector2(
+            anchors_E_W[1].position.x - anchorDelta, anchors_E_W[1].position.y); //set west
+
+        //set Right control points
+        controlPoints_R_L[0].position = new Vector2(
+            controlPoints_R_L[0].position.x + cPointDelta, anchors_N_S[0].position.y);
+        controlPoints_R_L[1].position = new Vector2(
+            controlPoints_R_L[1].position.x + cPointDelta, anchors_N_S[1].position.y);
+        //set Left control points
+        controlPoints_R_L[2].position = new Vector2(
+            controlPoints_R_L[2].position.x - cPointDelta, anchors_N_S[0].position.y);
+        controlPoints_R_L[3].position = new Vector2(
+            controlPoints_R_L[3].position.x - cPointDelta, anchors_N_S[1].position.y);
     }
 
-    private void HorizontalOrbitManipulation(float anchorDelta, float controlPointDelta)
+    private void VerticalOrbitAdjustment(float deltaTime)
     {
-        //left anchors
-        routes[0][0].Set(routes[0][0].x - anchorDelta, routes[0][0].y);
-        routes[3][3].Set(routes[3][3].x - anchorDelta, routes[3][3].y);
-        //right anchors
-        routes[1][3].Set(routes[1][3].x + anchorDelta, routes[1][3].y);
-        routes[2][0].Set(routes[2][0].x + anchorDelta, routes[2][0].y);
-        //left control points
-        routes[0][2].Set(routes[0][2].x - controlPointDelta, routes[0][2].y);
-        routes[3][1].Set(routes[3][1].x - controlPointDelta, routes[3][1].y);
-        //right control points
-        routes[1][1].Set(routes[1][1].x + controlPointDelta, routes[1][1].y);
-        routes[2][2].Set(routes[2][2].x + controlPointDelta, routes[2][2].y);
+        float anchorDelta = anchorPointMoveSpeed * deltaTime;
+        float cPointDelta = controlPointMoveSpeed * deltaTime;
+        
+        //set anchors
+        anchors_N_S[0].position = new Vector2(
+            anchors_N_S[0].position.x, anchors_N_S[0].position.y + anchorDelta); //set north
+        anchors_N_S[1].position = new Vector2(
+            anchors_N_S[1].position.x, anchors_N_S[1].position.y - anchorDelta); //set south
+
+        //set Up control points
+        controlPoints_U_D[0].position = new Vector2(
+            anchors_E_W[0].position.x, controlPoints_U_D[0].position.y + cPointDelta);
+        controlPoints_U_D[1].position = new Vector2(
+            anchors_E_W[1].position.x, controlPoints_U_D[1].position.y + cPointDelta);
+        //set Down control points
+        controlPoints_U_D[2].position = new Vector2(
+            anchors_E_W[0].position.x, controlPoints_U_D[2].position.y - cPointDelta);
+        controlPoints_U_D[3].position = new Vector2(
+            anchors_E_W[1].position.x, controlPoints_U_D[3].position.y - cPointDelta);
     }
 }
