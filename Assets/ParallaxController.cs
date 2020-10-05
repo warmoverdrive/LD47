ed120d6735502e5b5c601a9e11aabea970db784b
warmoverdrive <sweetsthebear@gmail.com>
2020-10-05 10:47:37 -0700

    Background Parallax Added

---
+++ Assets/ParallaxController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParallaxController : MonoBehaviour
+{
+    [SerializeField]
+    float scrollSpeed = 1f;
+    float repeatDistance = 32; //number of units on X axis before its time to move the background
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.x >= -32f)
+        {
+            transform.position += Vector3.left * scrollSpeed * Time.deltaTime;
+
+        }
+        else
+        {
+            transform.position = Vector3.zero;
+        }
+    }
+}
