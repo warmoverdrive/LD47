ae1096dabdfbf1c20b5e69b5d29a0272c5e97ecf
warmoverdrive <sweetsthebear@gmail.com>
2020-10-05 11:49:24 -0700

    pray to god

--- Assets/Scripts/ScoreController.cs
+++ Assets/Scripts/ScoreController.cs
@@ -4,21 +4,25 @@ using UnityEngine;
 
 public class ScoreController : MonoBehaviour
 {
+    private ScoreText scoreText;
     private int score;
     void Start()
     {
+        scoreText = FindObjectOfType<ScoreText>();
         ResetScore();
     }
 
     public void ResetScore()
 	{
         score = 0;
+        scoreText.UpdateText(score.ToString());
 	}
 
     public void StarHit()
 	{
         score += 1;
-	}
+        scoreText.UpdateText(score.ToString());
+    }
 
     public int GetScore()
 	{
---
+++ Assets/Scripts/ScoreText.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreText : MonoBehaviour
+{
+    private Text textBox;
+
+    private void Start()
+    {
+        textBox = GetComponent<Text>();
+    }
+
+    public void UpdateText(string text)
+    {
+        textBox.text = "Score: " + text;
+    }
+}
